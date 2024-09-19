d8935253c40f0f6ff170e582ffd52ae7d408940f
Aditya <aditya.chaudhary1558@gmail.com>
2024-09-19 14:14:53 +0530

    feat: Created the model for the domain

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -6,10 +6,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract ETHDaddy is ERC721 {
     address public owner;
 
+    struct Domain {
+        string name;
+        uint256 cost;
+        bool isOwned;
+    }
+
+    mapping(uint256 => Domain) domains;
+
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    function list(string memory _name, uint256 _cost) public {
+        Domain(_name, _cost, false);
     }
 }
